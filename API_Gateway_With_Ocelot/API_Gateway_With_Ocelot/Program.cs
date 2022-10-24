3adc041a1ff26c3de4b898142f40a28b10f63c6d
Phan Thanh Bình <thanhbinh07102001@gmail.com>
2022-10-24 15:04:03 +0700

    Adding web and get data by using by API gateway

---
+++ API_Gateway_With_Ocelot/API_Gateway_With_Ocelot/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_APIGateway;
+
+namespace API_Gateway_With_Ocelot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("First Main Function");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                logger.Error(ex);
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(s => s.AddJsonFile("configuration.json"));
+                })
+            .ConfigureLogging(logging => logging.AddConsole())
+            .UseNLog();
+                
+    }
+}
